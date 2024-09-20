b3c407e0043a1176ce35b312e9ece8b30491ac16
Babenko Valera <11112004valerababenko@gmail.com>
2024-09-20 19:57:21 +0300

    migrations for user app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users',
+    'identification',
+    
     "rest_framework",
     "corsheaders",
 ]
