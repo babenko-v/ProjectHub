cc871a882b2ae6cdbab745debdec6173405f5f7d
Babenko Valera <11112004valerababenko@gmail.com>
2024-09-22 23:33:45 +0300

    jwt on frontend

---
+++ frontend/src/services/auth.jsx
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export const login = async (email, password) => {
+    try {
+        const response = await axios.post('/api/token/', {
+            email,
+            password,
+        });
+
+        const { access } = response.data;
+
+        localStorage.setItem('access_token', access);
+        return true;
+    } catch (error) {
+        console.error('Ошибка при логине:', error);
+        throw error;
+    }
+};
+
+export const refreshAccessToken = async () => {
+    try {
+        const response = await axios.post('/api/token/refresh/');
+
+        const { access } = response.data;
+
+        localStorage.setItem('access_token', access);
+    } catch (error) {
+        console.error('Ошибка при обновлении токена:', error);
+        throw error;
+    }
+};
